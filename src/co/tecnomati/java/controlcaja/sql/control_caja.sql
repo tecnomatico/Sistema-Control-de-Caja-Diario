7416bf3f3d336795a68121f906b2efac6174f9d6
joel <joel@joel-dell>
2013-09-14 10:30:00 -0300

    bd agregado al proyecto

--- src/co/tecnomati/java/controlcaja/sql/control_caja.sql
+++ src/co/tecnomati/java/controlcaja/sql/control_caja.sql
@@ -1,9 +1,9 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.4
+-- version 4.0.4.1
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 11-08-2013 a las 15:39:09
+-- Tiempo de generación: 14-09-2013 a las 15:29:02
 -- Versión del servidor: 5.5.32
 -- Versión de PHP: 5.4.16
 
@@ -29,16 +29,81 @@ USE `control_caja`;
 --
 
 CREATE TABLE IF NOT EXISTS `asociado` (
-  `LEGAJO` int(11) NOT NULL,
-  `MATRICULA` int(11) DEFAULT NULL,
-  `CUIT` int(11) DEFAULT NULL,
-  `DNI` int(11) DEFAULT NULL,
-  `APELLIDO` varchar(100) COLLATE utf8_spanish_ci DEFAULT NULL,
-  `NOMBRE` varchar(100) COLLATE utf8_spanish_ci DEFAULT NULL,
-  `FECHA_INGRESO` date DEFAULT NULL,
-  `TELEFONO` varchar(20) COLLATE utf8_spanish_ci DEFAULT NULL,
-  PRIMARY KEY (`LEGAJO`),
-  KEY `MATRICULA` (`MATRICULA`)
+  `idAsociado` int(11) NOT NULL AUTO_INCREMENT,
+  `idCoop` int(11) DEFAULT NULL,
+  `legajo` int(11) NOT NULL,
+  `cuit` bigint(20) DEFAULT NULL,
+  `dni` int(11) DEFAULT NULL,
+  `apellido` varchar(100) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `nombre` varchar(100) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `ingreso` date DEFAULT NULL,
+  `telefono` varchar(20) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `estado` int(2) DEFAULT NULL,
+  PRIMARY KEY (`idAsociado`),
+  UNIQUE KEY `legajo` (`legajo`),
+  UNIQUE KEY `cuit` (`cuit`),
+  UNIQUE KEY `dni` (`dni`),
+  KEY `idCoop` (`idCoop`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=2 ;
+
+--
+-- Volcado de datos para la tabla `asociado`
+--
+
+INSERT INTO `asociado` (`idAsociado`, `idCoop`, `legajo`, `cuit`, `dni`, `apellido`, `nombre`, `ingreso`, `telefono`, `estado`) VALUES
+(1, NULL, 1, 1, 1, 'PELAEZ', 'JOEL', '2013-09-13', '', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente`
+--
+
+CREATE TABLE IF NOT EXISTS `cliente` (
+  `idCliente` int(11) NOT NULL AUTO_INCREMENT,
+  `cuit` bigint(20) NOT NULL,
+  `razonSocial` varchar(100) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `domicilio` varchar(200) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `estado` int(2) DEFAULT NULL,
+  PRIMARY KEY (`idCliente`),
+  UNIQUE KEY `cuit` (`cuit`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `comprobante`
+--
+
+CREATE TABLE IF NOT EXISTS `comprobante` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `numeroSerie` bigint(20) NOT NULL,
+  `cuit` bigint(20) DEFAULT NULL,
+  `tipoPersona` int(2) DEFAULT NULL,
+  `fecha` date DEFAULT NULL,
+  `codigoTipoComprob` int(11) DEFAULT NULL,
+  `tipoProceso` int(2) DEFAULT NULL,
+  `ejercicioEconomico` date DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `numeroSerie` (`numeroSerie`),
+  UNIQUE KEY `cuit` (`cuit`),
+  KEY `codigoTipoComprob` (`codigoTipoComprob`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `comprobanteconcepto`
+--
+
+CREATE TABLE IF NOT EXISTS `comprobanteconcepto` (
+  `id` int(11) NOT NULL,
+  `idComprobante` int(11) DEFAULT NULL,
+  `idConcepto` int(11) DEFAULT NULL,
+  `monto` double DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idConcepto` (`idConcepto`),
+  KEY `idComprobante` (`idComprobante`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 -- --------------------------------------------------------
@@ -48,10 +113,18 @@ CREATE TABLE IF NOT EXISTS `asociado` (
 --
 
 CREATE TABLE IF NOT EXISTS `concepto` (
-  `CODIGO` int(11) NOT NULL,
-  `DESCRIPCION` varchar(100) COLLATE utf8_spanish_ci DEFAULT NULL,
-  PRIMARY KEY (`CODIGO`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+  `codigoConcepto` int(11) NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(100) COLLATE utf8_spanish_ci DEFAULT NULL,
+  PRIMARY KEY (`codigoConcepto`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=3 ;
+
+--
+-- Volcado de datos para la tabla `concepto`
+--
+
+INSERT INTO `concepto` (`codigoConcepto`, `descripcion`) VALUES
+(1, 'monotributo'),
+(2, 'desarrollo de software a medida');
 
 -- --------------------------------------------------------
 
@@ -60,47 +133,19 @@ CREATE TABLE IF NOT EXISTS `concepto` (
 --
 
 CREATE TABLE IF NOT EXISTS `cooperativa` (
-  `MATRICULA` int(11) NOT NULL,
-  `CUIT` int(11) DEFAULT NULL,
-  `NOMBRE` varchar(100) COLLATE utf8_spanish_ci DEFAULT NULL,
-  `FECHA_INGRESO` date DEFAULT NULL,
-  `TELEFONO` varchar(20) COLLATE utf8_spanish_ci DEFAULT NULL,
-  `DOMICILIO` varchar(200) COLLATE utf8_spanish_ci DEFAULT NULL,
-  PRIMARY KEY (`MATRICULA`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `diario`
---
-
-CREATE TABLE IF NOT EXISTS `diario` (
-  `FECHA` date NOT NULL,
-  `SALDO_INICIAL` double DEFAULT NULL,
-  `ID_ULTIMO_RECIBO` int(11) DEFAULT NULL,
-  PRIMARY KEY (`FECHA`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `formulario`
---
-
-CREATE TABLE IF NOT EXISTS `formulario` (
-  `NUMERO_SERIE` int(11) NOT NULL,
-  `CUIT` int(11) DEFAULT NULL,
-  `FECHA` date DEFAULT NULL,
-  `CODIGO_CONCEPTO` int(11) DEFAULT NULL,
-  `CODIGO_FORMULARIO` int(11) DEFAULT NULL,
-  `MONTO` double DEFAULT NULL,
-  `NOMBRE` varchar(100) COLLATE utf8_spanish_ci DEFAULT NULL,
-  PRIMARY KEY (`NUMERO_SERIE`),
-  KEY `CODIGO_CONCEPTO` (`CODIGO_CONCEPTO`),
-  KEY `CODIGO_FORMULARIO` (`CODIGO_FORMULARIO`),
-  KEY `FECHA` (`FECHA`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+  `idCoop` int(11) NOT NULL AUTO_INCREMENT,
+  `matricula` int(11) NOT NULL,
+  `cuit` bigint(20) DEFAULT NULL,
+  `razonSocial` varchar(100) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `inicioActividad` date DEFAULT NULL,
+  `telefono` varchar(20) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `domicilio` varchar(200) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `inicioCaja` double DEFAULT NULL,
+  `ingresoBruto` varchar(100) COLLATE utf8_spanish_ci DEFAULT NULL,
+  PRIMARY KEY (`idCoop`),
+  UNIQUE KEY `matricula` (`matricula`),
+  UNIQUE KEY `cuit` (`cuit`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -109,24 +154,37 @@ CREATE TABLE IF NOT EXISTS `formulario` (
 --
 
 CREATE TABLE IF NOT EXISTS `proveedor` (
-  `CUIT` int(11) NOT NULL,
-  `RAZON_SOCIAL` varchar(100) COLLATE utf8_spanish_ci DEFAULT NULL,
-  PRIMARY KEY (`CUIT`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+  `idProveedor` int(11) NOT NULL AUTO_INCREMENT,
+  `cuit` bigint(20) NOT NULL,
+  `razonSocial` varchar(100) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `domicilio` varchar(200) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `estado` int(2) DEFAULT NULL,
+  PRIMARY KEY (`idProveedor`),
+  UNIQUE KEY `cuit` (`cuit`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `tipo_formulario`
+-- Estructura de tabla para la tabla `tipocomprobante`
 --
 
-CREATE TABLE IF NOT EXISTS `tipo_formulario` (
-  `CODIGO` int(11) NOT NULL,
-  `FORMULARIO` varchar(100) COLLATE utf8_spanish_ci DEFAULT NULL,
-  `TIPO` varchar(100) COLLATE utf8_spanish_ci DEFAULT NULL,
-  `ACCION` varchar(100) COLLATE utf8_spanish_ci DEFAULT NULL,
-  PRIMARY KEY (`CODIGO`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+CREATE TABLE IF NOT EXISTS `tipocomprobante` (
+  `codigo` int(11) NOT NULL AUTO_INCREMENT,
+  `formulario` varchar(100) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `referencia` varchar(20) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `detalleConcepto` varchar(20) COLLATE utf8_spanish_ci DEFAULT NULL,
+  PRIMARY KEY (`codigo`),
+  UNIQUE KEY `formulario` (`formulario`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=3 ;
+
+--
+-- Volcado de datos para la tabla `tipocomprobante`
+--
+
+INSERT INTO `tipocomprobante` (`codigo`, `formulario`, `referencia`, `detalleConcepto`) VALUES
+(1, 'RECIBO A', 'RA', NULL),
+(2, 'RECIBO B', 'RB', NULL);
 
 --
 -- Restricciones para tablas volcadas
@@ -136,15 +194,20 @@ CREATE TABLE IF NOT EXISTS `tipo_formulario` (
 -- Filtros para la tabla `asociado`
 --
 ALTER TABLE `asociado`
-  ADD CONSTRAINT `asociado_ibfk_1` FOREIGN KEY (`MATRICULA`) REFERENCES `cooperativa` (`MATRICULA`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `asociado_ibfk_1` FOREIGN KEY (`idCoop`) REFERENCES `cooperativa` (`idCoop`);
 
 --
--- Filtros para la tabla `formulario`
+-- Filtros para la tabla `comprobante`
 --
-ALTER TABLE `formulario`
-  ADD CONSTRAINT `formulario_ibfk_1` FOREIGN KEY (`CODIGO_CONCEPTO`) REFERENCES `concepto` (`CODIGO`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `formulario_ibfk_2` FOREIGN KEY (`CODIGO_FORMULARIO`) REFERENCES `tipo_formulario` (`CODIGO`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `formulario_ibfk_3` FOREIGN KEY (`FECHA`) REFERENCES `diario` (`FECHA`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `comprobante`
+  ADD CONSTRAINT `comprobante_ibfk_1` FOREIGN KEY (`codigoTipoComprob`) REFERENCES `tipocomprobante` (`codigo`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `comprobanteconcepto`
+--
+ALTER TABLE `comprobanteconcepto`
+  ADD CONSTRAINT `comprobanteconcepto_ibfk_1` FOREIGN KEY (`idConcepto`) REFERENCES `concepto` (`codigoConcepto`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `comprobanteconcepto_ibfk_2` FOREIGN KEY (`idComprobante`) REFERENCES `comprobante` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;

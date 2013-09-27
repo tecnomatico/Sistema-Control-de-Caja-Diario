90a23e0954b0d410ff435833dc4fa23fe903b429
AnahiAramayo <vanesa.aramayo@gmail.com>
2013-09-27 08:04:31 -0300

    en curso, completando  recibo
    --
    Sistema de Gestión de Control de Caja.
    Cooperativa de Trabajo Tecnomática Ltda. - San Salvador de Jujuy - Jujuy Argentina.
    www.tecnomati.co

--- src/co/tecnomati/java/controlcaja/reporte/jrdatasource/RboIntegCuotaSocialJRDataSource.java
+++ src/co/tecnomati/java/controlcaja/reporte/jrdatasource/RboIntegCuotaSocialJRDataSource.java
@@ -4,22 +4,76 @@
  */
 package co.tecnomati.java.controlcaja.reporte.jrdatasource;
 
+import co.tecnomati.java.controlcaja.dominio.Asociado;
+import co.tecnomati.java.controlcaja.dominio.dao.AsociadoDAO;
+import java.util.ArrayList;
+import net.sf.jasperreports.engine.JRDataSource;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JRField;
+import java.util.List;
+import co.tecnomati.java.controlcaja.util.*;
+
+import co.tecnomati.java.controlcaja.dominio.Comprobante;
+import co.tecnomati.java.controlcaja.dominio.Cooperativa;
+import co.tecnomati.java.controlcaja.dominio.dao.ComprobanteDAO;
+import co.tecnomati.java.controlcaja.dominio.dao.imp.ComprobanteDaoImp;
+
+
+
 /**
  *
  * @author AnahiAramayo
  */
 public class RboIntegCuotaSocialJRDataSource implements JRDataSource
 {
-    private List<Participante> listaParticipantes = new ArrayList<Participante>();
-    private int indiceParticipanteActual = -1;
+ private List<Comprobante> comprobante = new ArrayList<Comprobante>();
+    private int index = -1;//actual
+    Comprobante cc;
+    Cooperativa c;
     
     public Object getFieldValue(JRField jrf) throws JRException
     {
-        throw new UnsupportedOperationException("Not supported yet.");
+        Object valor = null;
+
+        if ("inicioActividad".equals(jrf.getName()))
+        {
+            valor = c.getInicioActividad();
+        }
+        else if ("nombreApellido".equals(jrf.getName()))
+        {
+            valor = comprobante.get(index).getUsername();
+        }
+        else if ("nroAsociado".equals(jrf.getName()))
+        {
+            valor = comprobante.get(index).getPassword();
+        }
+        else if ("fechaIngreso".equals(jrf.getName()))
+        {
+            valor = comprobante.get(index).getComentarios();
+        }
+        else if ("nroDNI".equals(jrf.getName()))
+        {
+            valor = comprobante.get(index).getComentarios();
+        }else if ("cuit".equals(jrf.getName()))
+        {
+            valor = comprobante.get(index).getComentarios();
+        }else if ("cantidadPago".equals(jrf.getName()))
+        {
+            valor = comprobante.get(index).getComentarios();
+        }else if ("fechaPago".equals(jrf.getName()))
+        {
+            valor = comprobante.get(index).getComentarios();
+        }
+        return valor;
     }
 
     public boolean next() throws JRException
     {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return ++index < comprobante.size();
+    }
+
+    public void addParticipante(Participante participante)
+    {
+        this.comprobante.add(participante);
     }
 }
